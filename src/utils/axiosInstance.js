6ba964960275df8ea176826053f77deafdc2ef13
prashant9420 <prashantpal2468@gmail.com>
2025-05-29 15:55:37 +0530

    backend Url changed

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { config } from '../config';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: config.backendUrl,
 });
 
 instance.interceptors.request.use((config) => {
